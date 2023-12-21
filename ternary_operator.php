3a7bc3d78ab6f6707354e0b233740f62b2744cc8
Divyanshi <142373656+Innovative-divya@users.noreply.github.com>
2023-12-21 20:56:58 +0530

    Add files via upload

---
+++ ternary_operator.php
@@ -0,0 +1,15 @@
+<?php
+//ternary operator 
+$x=9;
+($x>10)? $z="greater":$z="smaller";
+echo $z, "<br>";
+
+$a=32;
+($a<50)?$b="Value is less than 50":$b="Value is greater than 50";
+echo $b, "<br>";
+
+$p=10;
+$q= $p>20? "greater":"smaller";
+echo $z;
+
+?>
